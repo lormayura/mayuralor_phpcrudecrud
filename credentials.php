b7b279fd6d1bee86a5a481452e8532ef6b36c78d
lormayura <lormayura@gmail.com>
2025-11-12 01:52:28 +0000

    Other html files

--- credentials.php
+++ credentials.php
@@ -1,6 +1,7 @@
 <?php
+//populate data fields
 $servername = 'localhost';     //if apache on diff server from db or using Docker, set IP address
-$dbname = 'employees';              //which db you're going to use
-$username = 'phpuser1';
-$password = 'abc123';
+$dbname = '';              //which db you're going to use
+$username = '';
+$password = '';
 ?>
